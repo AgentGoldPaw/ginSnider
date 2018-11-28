1b9c8e2bd36031a6b055bbe8f89a959eeaabd41a
廖哲 <liaozhe@liaozhedeMacBook-Pro.local>
2018-11-28 12:38:43 +0800

    解决参数为1到2个时，以get form形式上传参数时的bug

--- utils/query_string.go
+++ utils/query_string.go
@@ -6,7 +6,7 @@ import (
 )
 
 // make query string from params
-func MakeQueryStrFrom(params interface{}) (result string) {
+func MakeQueryStrFrom(params interface{}) (paramCount int, result string) {
 	if params == nil {
 		return
 	}
@@ -21,10 +21,12 @@ func MakeQueryStrFrom(params interface{}) (result string) {
 				formName = GetCamelNameFrom(value.Type().Field(i).Name)
 			}
 			result += "&" + formName + "=" + fmt.Sprintf("%v", value.Field(i).Interface())
+			paramCount++
 		}
 	case reflect.Map:
 		for _, key := range value.MapKeys() {
 			result += "&" + fmt.Sprintf("%v", key.Interface()) + "=" + fmt.Sprintf("%v", value.MapIndex(key).Interface())
+			paramCount++
 		}
 	default:
 		return
